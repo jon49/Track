4c7b79cf5400cd74749b9f8d406f01361b3dd398
Jon Nyman <nymanjon@gmail.com>
2018-12-30 23:24:05 -0700

    Update urls

--- Server/src/Server/Teams/TeamsUrl.fs
+++ Server/src/Server/Teams/TeamsUrl.fs
@@ -3,19 +3,22 @@
 module Url =
 
     module Put =
-        let make =
+        let team =
             sprintf "/teams/%i/users/%i"
 
     module Get =
+        let team = sprintf "/teams/%i/users/%i"
         let all = "/teams"
         let latest = "/teams/latest"
 
     module Partial =
-        let userForm teamId userId =
-            sprintf "/teams/%i/users/%i/form" teamId userId
 
-        let addTeamButton =
-            "/teams/add"
+        let userForm teamId userId =
+            sprintf "/teams/%i/users/%i/edit" teamId userId
+
+        let addTeamButton = "/teams/add-button"
+
+        let addTeamForm = "/teams/add"
 
     module Post =
         let ``new`` = "/teams";
