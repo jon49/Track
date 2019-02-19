ff86985bddb800786b9e9f93ff0221732370a9a4
Jon Nyman <nymanjon@gmail.com>
2019-02-19 15:51:58 -0700

    Now I can add a new coach.

--- Server/src/Server/Features/FirstTime/FirstTimeController.fs
+++ Server/src/Server/Features/FirstTime/FirstTimeController.fs
@@ -26,7 +26,25 @@ module FirstTime =
             | None -> return! Controller.renderHtml ctx (div [] [ rawText "Welcome! You haven't been registered. Something wrong happened; Consider emailing me at nyman.jon+track@gmail.com" ])
         }
 
+    let newCoach (guid : Guid) : HttpHandler =
+        fun _ ctx ->
+        task {
+            match! User.getUserPermissions <| AspNet.getAuth0Id ctx with
+            | Some x ->
+                match! Model.getNewCoacTeamId guid x.UserId with
+                | Some x ->
+                    match x.HttpCode with
+                    | 200 ->
+                        return! (Controller.redirect ctx <| sprintf "/teams/%i" x.TeamId)
+                    | _ ->
+                        ctx.Response.StatusCode <- x.HttpCode
+                        return! Controller.renderHtml ctx (div [] [ rawText x.Message ])
+                | None -> return! Controller.renderHtml ctx (NotFound.layout)
+            | None -> return! Controller.renderHtml ctx (div [] [ rawText "Welcome! You haven't been registered. Something wrong happened; Consider emailing me at nyman.jon+track@gmail.com" ])
+        }
+
     let coordinator = router {
         getf "/coordinator/%O" newCoordinator
+        getf "/coach/%O" newCoach
     }
 
