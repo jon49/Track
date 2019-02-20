4aff6d0ff52c8d64c84487fe0ed1a79da40d44c3
Jon Nyman <nymanjon@gmail.com>
2019-02-20 16:22:36 -0700

    Add region teams relationship table

---
+++ DB/TrackDb/TrackDb/track/Tables/RegionTeam.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [track].[RegionTeam]
+( [TeamId] INT NOT NULL
+, [RegionId] INT NOT NULL
+, Active BIT NOT NULL DEFAULT 1
+, CONSTRAINT FK_RegionTeam_TeamId FOREIGN KEY (TeamId) REFERENCES [track].[Team] (TeamId)
+, CONSTRAINT FK_RegionTeam_RegionId FOREIGN KEY ([RegionId]) REFERENCES [track].[Region] ([RegionId])
+, CONSTRAINT PK_RegionTeam PRIMARY KEY (TeamId, [RegionId])
+)
