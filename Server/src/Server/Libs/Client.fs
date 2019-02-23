7bffe0f5220fae6186aaab0a1c7d59572b621cc9
Jon Nyman <nymanjon@gmail.com>
2019-02-23 08:43:46 -0700

    Custom validator

--- Server/src/Server/Libs/Client.fs
+++ Server/src/Server/Libs/Client.fs
@@ -3,29 +3,32 @@
 module Client =
     open System.ComponentModel.DataAnnotations
     open Giraffe.GiraffeViewEngine
-
-    let validate a =
-        let mutable _list = new System.Collections.Generic.List<ValidationResult>()
-        match Validator.TryValidateObject(a, new ValidationContext(a), _list) with
-        | true -> Ok a
-        | false -> Error <| System.String.Join ("\n", _list |> Seq.map (fun x -> x.ErrorMessage))
+    open System.Configuration
 
     let getDisplayName = Reflection.getDisplayName
 
-    let getFriendlyMessage (validationAttributes : ValidationAttribute[]) =
+    let getFriendlyMessage (validationAttribute : ValidationAttribute) =
+        match validationAttribute with
+        | :? StringLengthAttribute as x -> Some <| sprintf "must be at least %i or at most %i characters" x.MinimumLength x.MaximumLength
+        | :? EmailAddressAttribute -> Some "must be an email address"
+        | :? MinLengthAttribute as x -> Some <| sprintf "must be at least %i characters" x.Length
+        | :? MaxLengthAttribute as x -> Some <| sprintf "must be at most %i characters" x.Length
+        | :? RangeAttribute as x -> Some <| sprintf "must be between %O and %O" x.Minimum x.Maximum
+        | _ -> None
+
+    let getFriendlyMessages validationAttributes =
         validationAttributes
-        |> fun xs ->
-            xs
-            |> Seq.choose (fun x ->
-                match x with
-                | :? StringLengthAttribute as x -> Some <| sprintf "Must be at least %i or at most %i characters" x.MinimumLength x.MaximumLength
-                | :? EmailAddressAttribute -> Some "Must be an email address"
-                | :? MinLengthAttribute as x -> Some <| sprintf "Must be at least %i characters" x.Length
-                | :? MaxLengthAttribute as x -> Some <| sprintf "Must be at most %i characters" x.Length
-                | :? RangeAttribute as x -> Some <| sprintf "Must be between %O and %O" x.Minimum x.Maximum
-                | _ -> None
-            )
-            |> String.concat " &ndash; "
+        |> Array.choose getFriendlyMessage
+        |> String.concat " &ndash; "
+
+    let getFriendlyMessageWithValue (validationAttribute : ValidationAttribute, friendlyName, a) =
+        match validationAttribute with
+        | :? RequiredAttribute -> sprintf "'%s' value is required." friendlyName
+        | _ ->
+            match getFriendlyMessage validationAttribute with
+            | Some message -> sprintf "'%s' %s but given '%O'" friendlyName message a
+            | None -> ""
+            
 
     let getHtmlValidationAttributes (validationAttributes : ValidationAttribute[]) =
         validationAttributes
@@ -40,3 +43,24 @@ module Client =
                 | _ -> []
             )
             |> List.concat
+
+    let validate2 a =
+        let properties = a.GetType().GetProperties()
+        properties
+        |> Array.toList
+        |> List.choose (fun propertyInfo ->
+            let validationAttributes = Reflection.getCustomAttributes<ValidationAttribute> propertyInfo
+            let value = Reflection.getValue propertyInfo
+            match Reflection.SomeObj value with
+            | true, Some value
+            | false, Some value ->
+                validationAttributes
+                |> Array.tryFind ( fun x -> x.IsValid(value) )
+                |> Option.map (fun x -> x, Reflection.getDisplayName propertyInfo, Some value )
+            | false, None -> Some (RequiredAttribute() :> ValidationAttribute, Reflection.getDisplayName propertyInfo, None)
+            | true, None -> None
+        )
+        |> List.map getFriendlyMessageWithValue
+        |> function
+           | [] -> Ok a
+           | xs -> Error xs
