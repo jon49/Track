338f99aaac4edee01a10a531c17cf208b47766c0
Jon Nyman <nymanjon@gmail.com>
2019-03-31 13:00:26 -0700

    Added ability to prefetch urls.

--- Server/src/Server/static/scripts/app.js
+++ Server/src/Server/static/scripts/app.js
@@ -4,6 +4,10 @@
 
     var toArray = x => Array.prototype.slice.call(x),
         selectAll = x => document.querySelectorAll(x),
+        header = {
+            target: function (xhr) { return xhr.getResponseHeader('X-Target') },
+            action: function (xhr) { return xhr.getResponseHeader('X-Action') },
+        },
 
         // Constants
         attrDisabled = 'disabled',
@@ -11,124 +15,181 @@
 
         postSubmitActions = {
             Append: {
-                action: ($el, $target) => {
+                action: function ($el, $target) {
                     $target.append($el)
                 }
             },
             Replace: {
-                action: ($el, $target) => {
+                action: function ($el, $target) {
                     $target.parentNode.replaceChild($el, $target)
                 }
             },
             ReplaceIn: {
-                action: ($el, $target) => {
+                action: function ($el, $target) {
                     $target.replaceChild($el, $target)
                 }
             },
             Prepend: {
-                action: ($el, $target) => {
+                action: function ($el, $target) {
                     $target.prepend($el)
                 }
             }
         },
 
-        toggleClass = $el => {
-            var [toggle, target] = $el.getAttribute('data-toggle-class').split(',')
-            toArray(selectAll(target))
-                .forEach($target => {
-                    $target.classList.toggle(toggle)
-                })
+        // Load HTML
+        cache = {},
+        load = function (xhr, $el, options) {
+            options = options || {}
+            if (options.cacheResult) {
+                cache[xhr.url] = xhr
+                return
+            }
+            if (xhr.status !== 200 && xhr.status !== 304) {
+                // TODO show error and message
+                return
+            }
+
+            ; (options.custom && typeof options.custom === 'function' && options.custom())
+            var action, target;
+            if ((action = header.action(xhr))
+                && (target = header.target(xhr))
+                && xhr.responseText) {
+                var responsePage = document.createElement('buffer')
+                responsePage.innerHTML = xhr.responseText
+
+                toArray(responsePage.querySelectorAll(target))
+                    .forEach(function ($el) {
+                        var $target
+                        if ($target = document.getElementById($el.id)) {
+                            postSubmitActions[action].action($el, $target)
+                            $el.classList.add('loaded')
+                        }
+                    })
+            }
+
+            $el.classList.remove(classLoading)
+            scan()
         },
-        toggleAttribute = $el => {
-            var [attribute, target] = $el.getAttribute('data-toggle-attribute').split(',')
-            toArray(selectAll(target))
-                .forEach($target => {
-                    if ($target.hasAttribute(attribute)) {
-                        $target.setAttribute(attribute)
-                    } else {
-                        $target.removeAttribute(attribute)
-                    }
-                })
+
+        // Make pages instant
+        // TODO add try catch so if error falls back.
+        preloadXHR,
+        lastTouchTimestamp,
+        mouseoverTimer,
+        loadCache = function ($a) {
+            load(cache[$a.href], $a)
+            delete cache[$a.href]
+        },
+        preload = function ($a) {
+            var xhr = new XMLHttpRequest()
+            preloadXHR = xhr
+
+            xhr.withCredentials = true
+            xhr.onload = load.bind(null, xhr, $a, { cacheResult: true })
+            xhr.open('GET', $a.href, true)
+            xhr.send()
+        },
+        cancelPreload = function () {
+            preloadXHR && preloadXHR.abort()
+            preloadXHR = null
+        },
+        mouseoutListener = function() {
+            if (mouseoverTimer) {
+                clearTimeout(mouseoverTimer)
+                mouseoverTimer = null
+            }
+            else {
+                cancelPreload()
+            }
+        },
+        startHoverInstant = function ($el) {
+            if (performance.now() - lastTouchTimestamp < 1100) {
+                return
+            }
+
+            $el.addEventListener('mouseout', mouseoutListener, {passive: true})
+
+            mouseoverTimer = setTimeout(function() {
+                preload($el.href)
+                mouseoverTimer = null
+            }, 65)
+        },
+        startTouchInstant = function ($el) {
+            lastTouchTimestamp = performance.now()
+
+            $el.addEventListener('touchcancel', cancelPreload, {passive: true})
+            $el.addEventListener('touchend', cancelPreload, {passive: true})
+
+            preload($el.href)
+        },
+
+        // Events
+        eventList = [
+            ['form', 'data-form-registered', 'submit', false, submitForm ],
+            ['a.href', 'data-href-registered', 'mouseover', true, startHoverInstant ],
+            ['a.href', 'data-href-touch-registered', 'touchstart', true, startTouchInstant],
+            ['a.href', 'data-href-clicked-registered', 'click', false, loadCache]
+        ]
+
+        function submitForm($form) {
+            var $submit = $form.querySelector('[type=submit]'),
+                xhr = new XMLHttpRequest(),
+                baseUrl = $form.action,
+                acceptField = function (field) {
+                    return !!field.name
+                    && (field.type !== 'checkbox' && field.type !== 'radio' || field.checked)
+                    && (field.tagName !== 'BUTTON' || field === $submit)
+                },
+                queryString =
+                    toArray($form.elements)
+                        .filter(acceptField)
+                        .map(function (el) { return el.name + '=' + encodeURIComponent(el.value).replace(/%20/g, '+') })
+                        .join('&'),
+                getUrl = function() {
+                    var $url = document.createElement('a')
+                    $url.href = (url === '' ? document.URL : url) // IE bug workaround
+                    $url.search = queryString
+                    return $url.href
+                }
+
+            $submit.setAttribute(attrDisabled, '')
+            $submit.classList.add(classLoading)
+
+            xhr.withCredentials = true
+            xhr.onload = load.bind(null, xhr, $submit, {
+                custom: function () {
+                    xhr.getResponseHeader('X-Reset-Form') && $form.reset()
+                    !xhr.getResponseHeader('X-Disable-Submit') && $submit.removeAttribute(attrDisabled)
+                }
+            })
+            xhr.open($form.method, getUrl(), true)
+            xhr.send()
         }
 
-        // Submit form
-        ; ({
-            submitForm = $form => {
-                var $submit = $form.querySelector('[type=submit]'),
-                    xhr = new XMLHttpRequest(),
-                    baseUrl = $form.action,
-                    acceptField = field =>
-                        !!field.name
-                        && (field.type !== 'checkbox' && field.type !== 'radio' || field.checked)
-                        && (field.tagName !== 'BUTTON' || field === $submit),
-                    queryString =
-                        toArray($form.elements)
-                            .filter(acceptField)
-                            .map(el => el.name + '=' + encodeURIComponent(el.value).replace(/%20/g, '+'))
-                            .join('&'),
-                    getUrl = () => {
-                        var $url = document.createElement('a')
-                        $url.href = (url === '' ? document.URL : url) // IE bug workaround
-                        $url.search = queryString
-                        return $url.href
-                    }
+        function scan() {
+            eventList.forEach(function (xs) {
+                var query = xs[0],
+                    registered = xs[1],
+                    event = xs[2],
+                    passive = xs[3],
+                    listener = xs[4]
 
-                $submit.setAttribute(attrDisabled, '')
-                $submit.classList.add(classLoading)
-
-                xhr.withCredentials = true
-                xhr.onload = function processResponse() {
-                    if (xhr.status !== 200 && xhr.status !== 304) {
-                        // TODO show error and message
-                        return
-                    }
-
-                    if (xhr.getResponseHeader('X-Reset-Form')) {
-                        $form.reset()
-                    }
-                    if (!xhr.getResponseHeader('X-Disable-Submit')) {
-                        $submit.removeAttribute(attrDisabled)
-                    }
-                    var action, target;
-                    if ((action = xhr.getResponseHeader('X-Action'))
-                        && (target = xhr.getResponseHeader('X-Target'))
-                        && xhr.responseText) {
-                        var responsePage = document.createElement('buffer')
-                        responsePage.innerHTML = xhr.responseText
-
-                        toArray(responsePage.querySelectorAll(target))
-                            .forEach($el => {
-                                var $target
-                                if ($target = document.getElementById($el.id)) {
-                                    postSubmitActions[action].action($el, $target)
-                                    $el.classList.add('loaded')
+                toArray(selectAll(query+':not(['+registered+'])'))
+                    .forEach(el => {
+                        el.addEventListener(event,
+                            function (e)
+                            {
+                                if (!passive) {
+                                    e.preventDefault()
                                 }
-                            })
-                    }
+                                listener(e.target)
+                            }, { passive })
+                        el.setAttribute(registered, '')
+                    })
+            })
+        }
 
-                    $submit.classList.remove(classLoading)
-                    this.scan()
-                }
-                xhr.open($form.method, getUrl(), true)
-                xhr.send()
-            },
-            scan = () => {
-                [
-                    ['form', 'data-form-registered', 'submit', e => {
-                        e.preventDefault()
-                        submitForm(e.target)
-                    }],
-                    ['[data-toggle-class]', 'data-toggle-class-registered', 'click', e => toggleClass(e.target)],
-                    ['[data-toggle-attribute]', 'data-toggle-attribute-registered', 'click', e => toggleAttribute(e.target)]
-                ].forEach(([query, registered, event, listener]) => {
-                    toArray(selectAll(`${query}:not([${registered}])`))
-                        .forEach(el => {
-                            el.addEventListener(event, listener)
-                            el.setAttribute(registered, '')
-                        })
-                })
-            }
-        }).scan()
+        scan()
 
 }());
 
